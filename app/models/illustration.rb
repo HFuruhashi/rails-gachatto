7f1316b122c9d8f2ece8a09b78c0e8d67fc6fae2
HFuruhashi <37462956+HFuruhashi@users.noreply.github.com>
2024-11-09 16:55:42 +0900

    Update file validations in Illustration and Music models to include size constraints

--- app/models/illustration.rb
+++ app/models/illustration.rb
@@ -1,5 +1,5 @@
 class Illustration < Post
 	has_one_attached :file
 
-	validates :file, presence: true, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'] }
+	validates :file, presence: true, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'], size_range: 1..5.megabytes }
 end
