1dc640435bef454cd6564d4cd19c0f63d3262a5c
HFuruhashi <37462956+HFuruhashi@users.noreply.github.com>
2024-11-24 15:02:14 +0900

    Refactor file validations in Illustration and Music models; comment out size constraints and improve layout in posts index and navbar

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,5 +1,5 @@
 class Music < Post
 	has_one_attached :file
 
-	validates :file, presence: true, blob: { content_type: ['audio/mpeg', 'audio/mp3'], size_range: 1..10.megabytes }
+	# validates :file, presence: true, blob: { content_type: ['audio/mpeg', 'audio/mp3'], size_range: 1..10.megabytes }
 end
